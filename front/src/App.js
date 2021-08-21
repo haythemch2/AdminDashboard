b832ad68e19395a87d94eafba1349bbe01950673
Alaahouidheg <alahouidheg@gmail.com>
2021-08-21 11:00:05 +0100

    added

--- front/src/App.js
+++ front/src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hi</header>
+    <div className='App'>
+      <h1>hello</h1>
+      <header className='App-header'>Hi</header>
     </div>
   );
 }
